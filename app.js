0a9f0af0c0aec49e7d7fa4b1ce05abb1cafd0598
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-12-20 07:53:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+/* Created by Tivotal */
+
+let hourTxt = document.querySelector(".hour");
+let minTxt = document.querySelector(".min");
+let secTxt = document.querySelector(".sec");
+let labelTxt = document.querySelector(".label");
+
+setInterval(() => {
+  let date = new Date();
+  let hour = date.getHours();
+  let mins = date.getMinutes();
+  let sec = date.getSeconds();
+
+  let label;
+  label = hour < 12 ? "AM" : "PM";
+
+  hour = hour > 12 ? hour - 12 : hour;
+  hour = hour == 0 ? 12 : hour;
+  hour = hour < 10 ? "0" + hour : hour;
+
+  mins = mins < 10 ? "0" + mins : mins;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  hourTxt.innerText = hour;
+  minTxt.innerText = mins;
+  secTxt.innerText = sec;
+
+  labelTxt.innerText = label;
+}, 1000);
